b5d6e646b657cb675ca936075d854c592ee5bd3b
Paul Chaffanet <nakwenda@livebox.home>
2019-08-16 20:13:12 +0200

    Mise à jour pour la nouvelle API

---
+++ mona-ios/Application/RegisterLogin/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  mona-ios
+//
+//  Created by Paul Chaffanet on 2019-08-16.
+//  Copyright © 2019 Paul Chaffanet. All rights reserved.
+//
+
+import Foundation
